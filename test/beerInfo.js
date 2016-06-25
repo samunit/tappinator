e2659d846a402e4868190cd4502b6f97267d58c9
Alexander Welin <alexander@skye.no>
2016-06-25 09:41:51 +0200

    added test and npm - Leet

---
+++ test/beerInfo.js
@@ -0,0 +1,13 @@
+var expect = require("chai").expect;
+
+describe("Beer information", function(){
+  describe("Beer ABV value", function(){
+    it("will return alchol %", function(){
+
+    });
+  });
+
+  describe("Beer description", function(){
+
+  });
+});
