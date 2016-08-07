939108e210f92e8d01286120d3de357d58a2552c
kjohann <kris-90@live.no>
2016-08-07 17:12:33 +0200

    Trying to move app folder into node_modules when deploying to Azure
    Not sure if this works..

--- postinstall.js
+++ postinstall.js
@@ -2,12 +2,16 @@
 
 // the src path relative to node_module
 
-var srcpath = '../app';
-var dstpath = 'node_modules/app';
-var fs = require('fs');
-fs.readlink(dstpath, function (err, exists) {
-    // create the link only if the dest does not exist!
-    if (!exists) {
-        fs.symlinkSync(srcpath, dstpath, 'dir');
-    }
-});
+if(process.env.NODE_ENV !== 'production') {
+  var srcpath = '../app';
+  var dstpath = 'node_modules/app';
+  var fs = require('fs');
+  fs.readlink(dstpath, function (err, exists) {
+      // create the link only if the dest does not exist!
+      if (!exists) {
+          fs.symlinkSync(srcpath, dstpath, 'dir');
+      }
+  });
+} else {
+  console.log('Skipping postinstall in production mode');
+}
